c5b725dc3b4f987a8397498452e2bdaf61d034eb
Main Server <mainserver>
2014-10-18 20:36:53 +0000

    notelist works!

--- controllers/notes.js
+++ controllers/notes.js
@@ -5,19 +5,16 @@ exports.index = function(req, res) {
   var client = new Evernote.Client({token: req.user.oauth_token});
   var noteStore = client.getNoteStore();
   var noteFilter = new Evernote.NoteFilter;
-  var guid = req.param.guid;
-  console.log(guid);
+  var guid = req.params.guid;
   noteFilter.notebookGuid = guid; 
- 
- noteStore.findNotes(noteFilter, function(err, notes) {
+  NoteList = new Evernote.NoteList;
+ notes =  noteStore.findNotes(noteFilter, 0, 100,  function(err, notes) {
    console.log(notes);
    res.render('notes', { 
      title: 'Notes',
      notes: notes,
-
    });
  });
-
 
 };
 
