fc34a8d76471820fe98eb8ba6026c024de13e16a
petedavison93 <petedavison93@gmail.com>
2014-10-19 02:07:08 +0100

    Complete db.py (for now)

--- python/db.py
+++ python/db.py
@@ -5,15 +5,43 @@ dbPort = 27017
 
 client = MongoClient(dbIP, dbPort)
 db = client['pythonDB']
-hptable = db['hasheet']
-flashtable = db['flasheet']
-extable = db['extable']
 
-def addHash():
-	pass
+def addFlash(hashStr, flash):
+	flashTable = db['flash']	
+	flashTable.insert({"hashStr": hashStr, "flash": flash})
 
-def hashExists(para):
-	pass
+def addXtra(hashStr, data):
+	xtraTable = db['xtra']	
+	xtraTable.insert({"hashStr": hashStr, "data": data})
 
-def compareHash():
-	pass
+def addHash(hashStr):
+	hpTable = db['hp']	
+	hpTable.insert({"hashStr": hashStr})
+
+def getFlash(hashStr):
+	a = []
+	flashTable = db['flash']
+	for r in flashTable.find({"hashStr": hashStr}):
+		a.append(r['flash'])
+	return a
+
+def getXtra(hashStr):
+	a = []
+	xtraTable = db['xtra']
+	for r in xtraTable.find({"hashStr": hashStr}):
+		a.append(r['data'])
+	return a
+
+def hashExists(hashStr):
+	hpTable = db['hasheet']
+	r = hpTable.findOne({"hashStr": hashStr})
+	if r == None:
+		return False
+	else:
+		return True
+
+def compareHash(hashStr1, hashStr2):
+	if hash1 == hash2:
+		return True
+	else:
+		return False
